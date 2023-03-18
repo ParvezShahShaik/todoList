2dbaf49a58249dddc44d906a6cce0678d78b94d8
underdog <parvezshahshaik@outlook.com>
2023-03-18 17:44:47 +0000

    New Exercise

---
+++ bonus.py
@@ -0,0 +1,14 @@
+# Program to find the length of the string
+# Parvez Method
+# user_title = "Enter the name of the title: "
+
+title_name = input("Enter the name of the title")
+
+# print(title_name)
+print(len(title_name))
+
+# Tutors method
+text = input("Enter a title: ")
+length = len(text)
+
+print("The length of the title is:", length)
